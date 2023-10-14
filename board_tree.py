50a6e235848b95a3069982c2e1c5e669c09c42ba
ewokasi <kostyakov0203@mail.ru>
2023-10-14 22:26:38 +0300

    undone merging with book

--- board_tree.py
+++ board_tree.py
@@ -2,6 +2,9 @@ from telebot.types import InlineKeyboardMarkup, InlineKeyboardButton, InputMedia
 import telebot
 import server_con
 import json
+import text_editor
+import texts
+import personal_asks
 
 def push_to_users(msg ,id, call,bot):
     data = server_con.get_users_groups(id, call.from_user.username)
@@ -18,6 +21,7 @@ def get_user_groups(group):
 
 def branch(call, bot):
     text = "Попробуй еще раз, что-то пошло не так (\n"
+    content = text_editor.read_dict('texts')
     btns = []
     group = server_con.get_users_groups(user_id=call.from_user.id, username = call.from_user.username)
     print(group)
@@ -58,6 +62,138 @@ def branch(call, bot):
        
         return
         
+    
+    #начало ветвлений
+    if "menu" == call.data:
+        btns.append( [InlineKeyboardButton(text="БСК", callback_data="bsk_0"), InlineKeyboardButton(text="Студсоветы", callback_data="studsovet")]) 
+        btns.append( [ InlineKeyboardButton(text="Промежуточная аттестация", callback_data="session_0") , InlineKeyboardButton(text="Общаги", callback_data="hotels")])
+        btns.append( [InlineKeyboardButton(text="Частозадаваемые", callback_data="FAQ")])
+        btns.append([InlineKeyboardButton(text="Свой вопрос", callback_data="personal_ask")])
+        text = "Привет, я могу помочь тебе разобраться с тем, как все у нас в вузе устроено\n"
+        bot.edit_message_media(chat_id=call.message.chat.id, message_id=call.message.message_id,
+                           media=InputMedia(type='photo', media=open(f"logo.jpeg", "rb")))
+
+    if "bsk" in call.data:
+        btns, text= text_editor.gen_form(content["БСК"], "bsk", call)
+        btns.append([InlineKeyboardButton(text="Назад", callback_data="menu")]) 
+        bot.edit_message_media(chat_id=call.message.chat.id, message_id=call.message.message_id,
+                           media=InputMedia(type='photo', media=open(f"card.jpg", "rb")))
+    if "personal_ask" in call.data:
+            text = "Отправьте боту в чат команду /ask. Бот предложит вам задать ваш вопрос и передаст оператору. Ответ вы получите в этот чат."
+            btns.append( [InlineKeyboardButton(text="Назад", callback_data="menu")])   
+
+    elif "studsovet" in call.data:
+        text = "У каждого факультета в нашем вузе есть свой студсовет. Присоединяйся, ведь учиться вместе интересней. Ссылки на ВК ты можешь получить по кнопкам:"
+        btns.append([InlineKeyboardButton("Общий студсовет ГУАП", url='https://vk.com/studsovguap')])
+        btns.append([InlineKeyboardButton("Студсовет 1 Института", url='https://vk.com/guap_ssi_1'), InlineKeyboardButton("Студсовет 2 Института", url='https://vk.com/guap.ssi2')])
+        btns.append([InlineKeyboardButton("Студсовет 3 Института", url='https://vk.com/studsovet_uyutno_3'), InlineKeyboardButton("Студсовет 4 Института", url='https://vk.com/guap_ss4')])
+        btns.append([InlineKeyboardButton("Студсовет 6 Института", url='https://vk.com/shesterka_suai'), InlineKeyboardButton("Студсовет 8 Института", url="https://vk.com/guapstudsovet8")])
+        btns.append([InlineKeyboardButton("Студсовет ФПТИ", url='https://vk.com/ssi_fpti'), InlineKeyboardButton("Студсовет 12 Института", url='https://vk.com/12fak')])
+        btns.append([InlineKeyboardButton(text="Назад", callback_data="menu")]) 
+        bot.edit_message_media(chat_id=call.message.chat.id, message_id=call.message.message_id,
+                           media=InputMedia(type='photo', media=open(f"studsovet.jpeg", "rb")))
+    elif "session" in call.data:
+        btns, text = text_editor.gen_form(content["Промежуточная аттестация"],"session", call)
+        btns.append( [InlineKeyboardButton(text="Назад", callback_data="menu")]) 
+    
+
+    
+    elif "hotels" in call.data:
+        btns.append( [InlineKeyboardButton(text="№1, пр. Маршала Жукова, д. 24", callback_data="hotels_№1")]) 
+        btns.append( [InlineKeyboardButton(text="№2, ул. Передовиков, д. 13", callback_data="hotels_№2")]) 
+        btns.append( [InlineKeyboardButton(text="№3, ул. Варшавская, д. 8", callback_data="hotels_№3")]) 
+        btns.append( [InlineKeyboardButton(text="Вопросы", callback_data="hotels_faq")]) 
+        btns.append( [InlineKeyboardButton(text="Назад", callback_data="menu")]) 
+        text = "Всего у ГУАПа 3 общежития:"
+
+        if "hotels_№"  in call.data:
+            btns.clear()
+            i = int(call.data[-1])
+            text = texts.hotels[f"{i}"][0]
+            btns.append( [InlineKeyboardButton(text="Группа в вк", url=texts.hotels[f"{i}"][1])]) 
+            btns.append( [InlineKeyboardButton(text="Больше фоток", url=texts.hotels[f"{i}"][2])]) 
+            btns.append( [InlineKeyboardButton(text="Назад", callback_data="hotels")]) 
+            bot.edit_message_media(chat_id=call.message.chat.id, message_id=call.message.message_id,
+                           media=InputMedia(type='photo', media=open(texts.hotels[f"{i}"][3], "rb")))
+
+        if "hotels_faq" == call.data:
+            btns.clear()
+            text = texts.hotels["faq"][0]
+            btns.append( [InlineKeyboardButton(text="1/2 >>", callback_data="hotels_faq_1")]) 
+            btns.append( [InlineKeyboardButton(text="Назад", callback_data="hotels")]) 
+        if "hotels_faq_1" == call.data:
+            btns.clear()
+            text = texts.hotels["faq"][1]
+            btns.append( [InlineKeyboardButton(text="2/2 <<", callback_data="hotels_faq")]) 
+            btns.append( [InlineKeyboardButton(text="Назад", callback_data="hotels")]) 
+    
+    elif "FAQ" in call.data:
+        btns.append( [InlineKeyboardButton(text="Контакты деканатов", callback_data="FAQ_dec") ,InlineKeyboardButton(text="Военная кафедра", callback_data="FAQ_voenka_0")]) 
+        btns.append( [InlineKeyboardButton(text="Про стипендию", callback_data="FAQ_stipendiya_0"), InlineKeyboardButton(text="История ГУАП", callback_data="FAQ_history_0")])
+        btns.append( [InlineKeyboardButton(text="Перевод на бюджет", callback_data="FAQ_перевод_на_бюджет_0")])  
+        btns.append( [InlineKeyboardButton(text="Назад", callback_data="menu")]) 
+    
+        text = "Раздел FAQ"
+
+        if "stipendiya" in call.data:
+            btns, text = text_editor.gen_form(content["Стипендия"], "FAQ_stipendiya", call)
+            btns.append( [InlineKeyboardButton(text="Назад", callback_data="FAQ")])
+          
+        if "перевод_на_бюджет" in call.data:
+            btns, text = text_editor.gen_form(content["Перевод на бюджет"],"FAQ_перевод_на_бюджет", call)  
+            btns.append( [InlineKeyboardButton(text="Назад", callback_data="FAQ")])
+         
+        
+        if "dec" in call.data:
+            btns.clear()
+            btns.append( [InlineKeyboardButton(text="1", callback_data="FAQ_dec_1"), InlineKeyboardButton(text="2", callback_data="FAQ_dec_2"), InlineKeyboardButton(text="3", callback_data="FAQ_dec_3")])
+            btns.append( [InlineKeyboardButton(text="4", callback_data="FAQ_dec_4"), InlineKeyboardButton(text="6", callback_data="FAQ_dec_6"), InlineKeyboardButton(text="8", callback_data="FAQ_dec_8")])
+            btns.append( [InlineKeyboardButton(text="ФПТИ", callback_data="FAQ_dec_ФПТИ"), InlineKeyboardButton(text="12", callback_data="FAQ_dec_12")])
+            btns.append( [InlineKeyboardButton(text="Назад", callback_data="FAQ")])
+            text =f"Выбирай нужный тебе"
+            if "FAQ_dec_" in call.data:
+                btns.clear()
+                inst = call.data.split(sep="_")[-1]
+                text = content[f"Деканат {inst}"]
+                btns.append( [InlineKeyboardButton(text="Назад", callback_data="FAQ_dec")])
+
+        if "FAQ_voenka" in call.data:
+            btns, text = text_editor.gen_form(content["ВУЦ"], "FAQ_voenka", call)
+            btns.append( [InlineKeyboardButton(text="Назад", callback_data="FAQ")])
+
+        if "FAQ_history" in call.data:
+            btns, text = text_editor.gen_form(content["История ГУАП"],"FAQ_history", call )
+            btns.append( [InlineKeyboardButton(text="Назад", callback_data="FAQ")])
+            bot.edit_message_media(chat_id=call.message.chat.id, message_id=call.message.message_id,
+                           media=InputMedia(type='photo', media=open('castle.jpeg', "rb")))
+        
+    elif "answer_on_message" in call.data:
+            
+            try:
+                questions = personal_asks.read_dict("personal_asks")
+                id = call.data.split(sep = "_")[-1]
+                
+                for question in questions["questions"]:
+                    if int(id) == int(question["id"]):
+                        break
+                
+                if question["status"]=="answered":
+                    bot.answer_callback_query(call.id, "На этот вопрос уже был получен ответ. Все отлично", show_alert=True)
+                    
+                else:
+                    msg = bot.reply_to(call.message, 'Ответчаете')
+                    bot.register_next_step_handler(msg, personal_asks.to_user, question["chat_id"], bot)
+                
+                    for question in questions["questions"]:
+                        if int(id) == int(question["id"]):
+                            question['status'] = "answered"
+                            break
+                    personal_asks.save_dict(questions, "personal_asks")
+            except:
+                bot.send_message(call.chat.id, f"Что-то пошло не так. username, чтобы ответить лично: {question['id']}")
+            return
+    
+    
     if "refresh" in call.data:
          btns.append([InlineKeyboardButton(text="Сдал", callback_data=f"done_{call.data.split(sep='_')[1]}")])
          btns.append([InlineKeyboardButton(text="На новый круг", callback_data=f"ring_{call.data.split(sep='_')[1]}")])
@@ -89,6 +225,12 @@ def branch(call, bot):
         return
 
     
+    try:
+        keyboard = InlineKeyboardMarkup(btns, row_width=2)
+        bot.edit_message_caption(chat_id=call.message.chat.id, message_id=call.message.message_id,caption = text, reply_markup=keyboard)
 
-    keyboard = InlineKeyboardMarkup(btns, row_width=2)
-    bot.send_message(chat_id=call.message.chat.id, text=text, reply_markup=keyboard)
+    except :
+        print("blaaaaaaa")
+                
+        keyboard = InlineKeyboardMarkup(btns, row_width=2)
+        bot.send_message(chat_id=call.message.chat.id, text=text, reply_markup=keyboard)  
--- main.py
+++ main.py
@@ -5,10 +5,87 @@ from telebot.types import InlineKeyboardMarkup, InlineKeyboardButton, InputMedia
 import board_tree
 import json
 bot=telebot.TeleBot(token=cfg.TOKEN)
+import personal_asks
+from datetime import date
+import text_editor
 
 
 
-@bot.message_handler(commands=['start'])
+def save_dict(dict, name):
+    json.dump(dict, open(str(name) + '.json', 'w', encoding='utf-8'), indent=2, ensure_ascii=False)
+
+def read_dict(name):
+    with open(str(name) + '.json', encoding='utf-8') as fh:
+        data = json.load(fh)
+    return data
+
+@bot.message_handler(commands=['ask'])
+def send_welcome(message):
+    msg = bot.reply_to(message, "Вы можете отправить вопрос в этот чат, после чего вам ответит оператор")
+    bot.register_next_step_handler(msg, personal_asks.get_question, bot)
+
+@bot.message_handler(commands=['joinCrew'])
+def add_to_FAQCrew(message):
+    personal_asks.add_operator(message, bot)
+
+@bot.message_handler(commands=['exitCrew'])
+def exit_FAQCrew(message):
+    personal_asks.remove_operator(message, bot)
+
+
+@bot.message_handler(commands=["start"])
+def start_answer(message):
+    bot.send_message(message.chat.id, "/Твой_Справочник\n/Хочу_в_Очередь_на_сдачу")
+
+@bot.message_handler(commands=['Твой_Справочник'])
+def book_answer(message):
+    keyboard = InlineKeyboardMarkup(row_width=2)
+    for_newby_btn = InlineKeyboardButton(text="БСК", callback_data="bsk_0")
+    studsovet_btn = InlineKeyboardButton(text="Студсовет", callback_data="studsovet")
+    classrooms_btn = InlineKeyboardButton(text="Промежуточная аттестация", callback_data="session_0")
+    hotels_btn =  InlineKeyboardButton(text="Общаги", callback_data="hotels")
+    FAQ_btn =  InlineKeyboardButton(text="Частозадаваемые", callback_data="FAQ")
+    personal_ask_btn = InlineKeyboardButton(text="Свой вопрос", callback_data="personal_ask")
+    keyboard.add(for_newby_btn, studsovet_btn,classrooms_btn, hotels_btn, FAQ_btn, personal_ask_btn)
+    bot.send_photo(message.chat.id, photo = open(f"logo.jpeg", "rb"), caption= "Привет, я могу помочь тебе разобраться с тем, как все у нас в вузе устроено\n", reply_markup=keyboard)
+
+
+@bot.message_handler(commands=["addAdmin"])
+def add_admin(message):
+    file = open(r"admins.txt")
+    admins = file.read().splitlines()
+    file.close()
+    if message.chat.username in admins:
+        file = open(r"admins.txt", "a")
+        file.write("\n"+message.text.replace("/addAdmin ", "", 1))
+        file.close()
+    bot.send_message(message.chat.id,  "Добавлен новый админ "+ message.text.replace("/addAdmin ", "", 1))
+
+
+@bot.message_handler(commands=["help"])
+def help_answer(message):
+    file = open(r"admins.txt")
+    admins = file.read().splitlines()
+    file.close()
+    if message.chat.username in admins:
+    
+        bot.send_message(message.chat.id, text = """В боте есть две роли: Админы и операторы. Админы могут редактировать содержимое справочника и добавлять других админов, а операторы могут отвечать на вопросы пользователей.\nЧтобы добавить Админа укажите его юзернейм без @ - "/addAdmin Anton"\n Чтобы стать/перестать быть оператором - "/joinCrew" / "/exitCrew"\nчтобы обновить информацию в боте отправьте в чат docx документ (очистив всё форматирование) с определенным названием:\n
+Для бск - "БСК.docx"\n
+Для Промежуточной аттестации - "Промежуточная аттестация.docx"\n
+Для Перевода на другой факультет -"Перевод на другой факультет.docx\"
+Для матпомощи - "Матпомощь.docx\"\n
+Для стипендии - "Стипендия.docx\"\n
+Для промежуточной аттестации - "Промежуточная аттестация.docx"\n
+Для ВУЦ - "ВУЦ.docx"\n
+Для перевода на бюджет = "Перевод на бюджет.docx"\n
+Для истории ГУАП - "История ГУАП"\n
+Для Обновления инфы о деканатах - "Деканат 1.docx" или "Деканат 2.docx","Деканат ФПТИ.docx","Деканат ВУЦ.docx"\n""")
+    else:
+         bot.send_message(message.chat.id, text = 'Чтобы начать работу бота - пиши "/start"\nЧтобы задать вопрос оператору - "/ask"')
+
+
+
+@bot.message_handler(commands=['Хочу_в_Очередь_на_сдачу'])
 def start_answer(message):
     btns = []
 
@@ -38,6 +115,31 @@ def start_answer(call):
 def callback_inline(call):
     board_tree.branch(call, bot)
 
+
+
+@bot.message_handler(content_types=['document', 'file']) # list relevant content types
+def edit_info(message):
+    file = open(r"admins.txt")
+    admins = file.read().splitlines()
+    file.close()
+    if message.chat.username not in admins: return
+    file_name = message.document.file_name
+    if file_name.split(sep = ".")[1]!="docx":
+        print("denied")
+        return 1
+    
+    file_info = bot.get_file(message.document.file_id)
+    downloaded_file = bot.download_file(file_info.file_path)
+    with open(file_name, 'wb') as new_file:
+        new_file.write(downloaded_file)
+
+    multistring= text_editor.getText(file_name)
+    print("------------------multistring-------------",multistring)
+    text_editor.update_text(file_name.split(sep = ".")[0], multistring)
+    
+
+    
+    bot.send_message(message.chat.id, text=file_name + ": сохранена информация")
 
 
 while 1:
